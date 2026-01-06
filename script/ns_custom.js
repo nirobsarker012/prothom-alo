fc09ff832425e721b6bbf4e9520eeab35544d0fe
nirob012 <nirobsarker012@gmail.com>
2026-01-07 01:25:07 +0600

    add: life-style-slider

--- script/ns_custom.js
+++ script/ns_custom.js
@@ -38,3 +38,44 @@ class NSVideoSlider extends HTMLElement {
 if (!customElements.get("ns-video-slider")) {
   customElements.define("ns-video-slider", NSVideoSlider);
 }
+
+class NSLifeStyleSlider extends HTMLElement {
+  connectedCallback() {
+    this.swiper = new Swiper(".nsLifeStyle", {
+      slidesPerView: 1,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      spaceBetween: 10,
+      loop: true,
+      pagination: {
+        el: ".ns_slide_dots",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 14,
+        },
+        1024: {
+          slidesPerView: 4.5,
+          spaceBetween: 20,
+        },
+      },
+      navigation: {
+        nextEl: ".ns_life_slide_right_btn",
+        prevEl: ".ns_life_slide_left_btn",
+      },
+    });
+  }
+  disconnectedCallback() {}
+}
+
+if (!customElements.get("ns-life-style-slider")) {
+  customElements.define("ns-life-style-slider", NSLifeStyleSlider);
+}
